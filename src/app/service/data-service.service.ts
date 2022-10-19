c3ac090357d8f59cc06b2723274b55cdb929cd81
Nikhil <nikhil.appukuttan@prvak.in>
2022-10-19 10:46:22 +0530

    first stage completed

---
+++ src/app/service/data-service.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataServiceService {
+  private dataSource: BehaviorSubject<string> = new BehaviorSubject<string>('initial data');
+  data: Observable<string> = this.dataSource.asObservable(); 
+
+  constructor() { }
+
+  sendData(data: string) {
+    this.dataSource.next(data);
+  }
+
+}
